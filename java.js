e51ed748fe25019ce3e7dc8d3b97c06462b37bbf
etedw <92881448+smlnf@users.noreply.github.com>
2023-01-17 15:47:33 -0500

    Create java.js

---
+++ java.js
@@ -0,0 +1,29 @@
+var img= []
+  img[0]="estro"
+  img[1]="sky_top_left"
+
+//Select all elements on the page with the name attribute equal to VCRImage
+var images = document.querySelectorAll('[name=VCRImage]');
+
+//For each image bind the click event
+for(var i=0; i < images.length; i++)
+{
+  var image = images[i];
+  //https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
+  image.addEventListener('click', imageClicked(), false);
+}
+
+function imageClicked()
+{
+  //Use a closure to wrap the counter variable
+  //so each image element has their own unique counter
+  var counter = 0;
+  return function(event)
+  {
+    //Increment counter
+    counter++;
+    //The context of "this" is the image element
+    //Use a modulus
+    this.src = img[counter % img.length];
+  }
+}
